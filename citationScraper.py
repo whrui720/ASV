12202375236887c5cdae86542a36776fd26a3662
Yuchen Wang <wangyuch@umich.edu>
2025-01-06 21:08:56 -0500

    citation scraper added

---
+++ citationScraper.py
@@ -0,0 +1,23 @@
+import PyPDF2
+
+def extract_pdf_text(pdf_path):
+    pdf_text = ''
+    with open(pdf_path, 'rb') as pdf_file:
+        pdf_reader = PyPDF2.PdfReader(pdf_file, strict = False)
+
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
+            pdf_text += page.extract_text()
+
+
+            pdf_text += ' '.join(pdf_text.split()) + ' '
+    return pdf_text
+
+
+if __name__ == '__main__':
+    pdf_path = 'SecureLoad.pdf'
+    pdf_text = extract_pdf_text(pdf_path)
+
+    with open("output.txt", "w") as file:
+        file.write(pdf_text)
+
