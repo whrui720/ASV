b6c3aa4d47b80a4f9fb2b22f3ae1c683caeda302
Yuchen Wang <wangyuch@umich.edu>
2025-03-08 13:51:01 -0500

    ref scraper done

--- citationScraper.py
+++ citationScraper.py
@@ -1,23 +1,86 @@
-import PyPDF2
+import fitz  # PyMuPDF
+from collections import defaultdict
+import re
 
-def extract_pdf_text(pdf_path):
-    pdf_text = ''
-    with open(pdf_path, 'rb') as pdf_file:
-        pdf_reader = PyPDF2.PdfReader(pdf_file, strict = False)
+references_dict = defaultdict(list)
 
-        for page_num in range(len(pdf_reader.pages)):
-            page = pdf_reader.pages[page_num]
-            pdf_text += page.extract_text()
+def extract_text(pdf_path):
+    """
+    Extracts text from all pages of a PDF file.
+    """
+    full_text = ""
+
+    with fitz.open(pdf_path) as doc:
+        for page in doc:
+            full_text += page.get_text() + "\n"
+    return full_text
+
+def locate_reference(full_text):
+    start_index = full_text.lower().find("references")
+
+    if start_index != -1:
+        references_text = full_text[start_index:]
+    else:
+        references_text = "References section not found."
+
+    # TODO: FIND BETTER WAY TO DETERMINE ENDING
+    return references_text[:2000]
 
 
-            pdf_text += ' '.join(pdf_text.split()) + ' '
-    return pdf_text
+def parse_reference(ref_text):
+    """
+    Parse a single string of references into dictionary of ref val : ref line
+    """
+
+    references_dict = {}
+    current_number = None
+    current_ref = []
+
+    # MATCH: "1.", "2."
+    reference_pattern = re.compile(r"^(\d+)\.\s(.+)")  
+
+    for line in ref_text.split("\n"):
+        line = line.strip()
+
+        # Check if the line starts with a reference number
+        match = reference_pattern.match(line)
+        if match:
+            if current_number is not None:
+                references_dict[current_number] = " ".join(current_ref).strip()
+
+            current_number = int(match.group(1)) 
+            current_ref = [match.group(2)]
+        else:
+            if current_number is not None:
+                current_ref.append(line)
+
+    if current_number is not None:
+        references_dict[current_number] = " ".join(current_ref).strip()
+
+    return references_dict
+    
+    
+
+
+def main():
+    pdf_path = 'papers/data_sharing.pdf'
+    doc = fitz.open(pdf_path)
+    text = "\n".join([page.get_text("text") for page in doc])
+    
+
+    ref_text = locate_reference(text)
+
+    ref_dict = parse_reference(ref_text)
+
+    with open('output.txt', 'w') as file:
+        for key, ref in ref_dict.items():
+            file.write(f"KEY: {key} |||| VAL: {ref}\n\n")
+            # file.write(ref_text)
+
 
 
 if __name__ == '__main__':
-    pdf_path = 'papers/'
-    pdf_text = extract_pdf_text(pdf_path)
+    main()
 
-    with open("output.txt", "w") as file:
-        file.write(pdf_text)
 
+    
